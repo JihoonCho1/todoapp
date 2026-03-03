698581db910f9ce44c00fa4cd82b9b539e89257f
JihoonCho1 <111573550+JihoonCho1@users.noreply.github.com>
2026-03-03 16:00:58 +1300

    Basic home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import whitelogo from './whitelogo.png';
+import React, { useEffect, useRef, useState } from 'react';
 
 function App() {
+  const ref = useRef(null);
+  const [active, setActive] = useState(false);
+
+  // Navbar change when observe a div
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setActive(true);
+        } else {
+          setActive(false); 
+        }
+      },
+      { threshold: 0.3 }
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => observer.disconnect();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={active ? "visible" : "NavBar"}>
+        <img className="logo" src={whitelogo} alt="logo"/>
+        <ul>
+          <li><a href="index.html">Login</a></li>
+          <li><a className="starthere" href="index.html">Get Started</a></li>
+        </ul>
+      </div>
+
+      <div className="Intro">
+        <p className="smallText">To Do with DoDo!</p>
+        <p className="prompt">Plan Better <br></br>Because<br></br>Today Matters</p>
+        <button id="startbtn">Get Started</button>
+      </div>
+
+      <div className="bodyobserver" ref={ref}>
+        <p>Maintainenace</p>
+      </div>
+
     </div>
   );
+
 }
 
 export default App;
