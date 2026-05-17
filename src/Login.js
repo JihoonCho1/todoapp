a7f5afd01755b38a93d12098174b3ac40cad5953
David <jihunchoe075@gmail.com>
2026-05-18 02:10:39 +1200

    Login button required inputs / hover / cursor updated

--- src/Login.js
+++ src/Login.js
@@ -6,6 +6,18 @@ import logo from './logo/black_logo_text.svg';
 
 function Login({ onClose }) {
   const [coords, setCoords] = useState({ x: 0, y: 0 });
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const isFormValid = email.trim() !== '' && password.trim() !== '';
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isFormValid) {
+      console.log("Login attempt: ", email, password);
+    }
+  }
+  
   return (
     
     <div className="login-page-wrapper">
@@ -19,15 +31,15 @@ function Login({ onClose }) {
               {/* Login UI code */}
               <div className="email-group">
                 <p id="login-text">EMAIL ADDRESS</p>
-                <input type="text" placeholder="name@example.com" className="login-input"></input>
+                <input type="text" required placeholder="name@example.com" className="login-input" onChange={(e) => setEmail(e.target.value)}></input>
               </div>
               <div className="password-group">
                 <p id="login-text">PASSWORD</p>
-                <input type="password" placeholder="Password" className="login-input"></input>
+                <input type="password" required placeholder="password" className="login-input" onChange={(e) => setPassword(e.target.value)}></input>
               </div>
               <div className="login-button-container">
-                <button id="login-button">
-                  Login
+                <button id="login-button" type="submit" disabled={!isFormValid} onClick={handleSubmit}>
+                  <span>LOG IN</span>
                 </button>
               </div>
           </div>
