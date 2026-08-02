755cdd979d2a3d1309e13d4eecb0737dd6ad59f6
David <jihunchoe075@gmail.com>
2026-08-03 01:35:22 +1200

    Emoji picker pops up when the emoji icon is clicked

--- frontend/src/components/EditStatus.js
+++ frontend/src/components/EditStatus.js
@@ -7,38 +7,68 @@ import EmojiPicker from 'emoji-picker-react';
 function EditStatus({isOpen, user, setUser, onClose}) {
     const [text, setText] = useState(user.statusMessage);
     const [showPicker, setShowPicker] = useState(false);
+    const [emoji, setEmoji] = useState(user.statusEmoji);
     const statusLength = text.length;
 
     if (!isOpen) return null;
 
 
     return (
-        <div className="edit-status-overlay">
-            <div className="status-contents">
-                <div className="header-contents">
-                    <h2>Edit Status</h2>
-                    <RxCross1 id="close-button" onClick={onClose}/>
-                </div>  
-                
-                <div className="status-message-field">
-                    <p className="input-text">Status Message:</p>
-                    <div className="input-box-wrapper">
-                        <input 
-                            className="edit-message" 
-                            type="text" 
-                            placeholder="What are you thinking?"
-                            value={text}
-                            maxLength={40}
-                            onChange={(e) => setText(e.target.value)}
-                        />
-                        <div className="emoji-icon">
-                            {user.statusEmoji ? user.statusEmoji : <BsEmojiSmile id="emoji-icon"/>}
-                        </div>
-                        
-                    </div>
-                    <p id="count-chars">{statusLength}/40</p>
-                </div>
+        <div className="overlay">
+            <div className="emoji-picker-container">
+                {showPicker && (
+                        <>
+                            <EmojiPicker
+                                onEmojiClick={(emojiData) => {
+                                    setEmoji(emojiData.emoji);
+                                    setShowPicker(false);
+                                }}
+                            />
+                        </>
+                    )}
             </div>
+            <div className="edit-status-overlay">
+                
+                <div className="status-contents">
+                    
+                    <div className="header-contents">
+                        <h2>Edit Status</h2>
+                        <RxCross1 id="close-button" onClick={onClose}/>
+                    </div>  
+                    
+                    <div className="status-message-field">
+                        <p className="input-text">Status Message:</p>
+                        <div className="input-box-wrapper">
+                            <input 
+                                className="edit-message" 
+                                type="text" 
+                                placeholder="What are you thinking?"
+                                value={text}
+                                maxLength={40}
+                                onChange={(e) => setText(e.target.value)}
+                            />
+                            <div className="emoji-icon" onClick={() => setShowPicker(!showPicker)}>
+                                {emoji ? emoji : <BsEmojiSmile id="emoji-icon"/>}
+                            </div>
+                            
+                        </div>
+                        <p id="count-chars">{statusLength}/40</p>
+                        <button className="save-button" onClick={() => {
+                            // Temporarily store the updated status message and emoji in the user state
+                            // need to use API to update the user status in the backend
+                            setUser({
+                                ...user,
+                                statusMessage: text,
+                                statusEmoji: emoji
+                            });
+                            onClose();
+                        }}>Save</button>
+                    </div>
+                    
+                </div>
+                
+            </div>
+
         </div>
     )
 }
